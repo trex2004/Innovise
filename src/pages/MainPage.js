8dacd40c91f66430cbf240194917f5107f0ae4cd
Swayam <98643120+trex2004@users.noreply.github.com>
2024-10-01 15:30:23 +0530

    main page skeleton commit

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -3,8 +3,16 @@ import React from "react";
 const MainPage = () =>{
     return(
         <>
-            <div>
-                <p>Main Page</p>
+            <div className="d-flex my-4 mx-3 justify-content-evenly responsive-flex ">
+                <div className="bg-warning w-25 d-flex justify-content-center mx-2">
+                    <p>hello</p>
+                </div>
+                <div className="bg-primary w-50 d-flex justify-content-center mx-2">
+                    <p>hello</p>
+                </div>
+                <div className="bg-success w-25 d-flex justify-content-center mx-2">
+                    <p>hello</p>
+                </div>
             </div>
         </>
     )
