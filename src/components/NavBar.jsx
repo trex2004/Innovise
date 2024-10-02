3b3413f7954441c2a89cea528fd36516d94a632e
Prthm444 <prathameshvasaikarat3x4@gmail.com>
2024-10-02 22:13:19 +0530

    logo and color improvement in side-navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import { Button } from "@mui/material"
 import "./NavBar.css"
-import innoviseLogo from "./innovise_logo_navbar.jpg"
+import innoviseLogo from "./innovise_logo.png"
 import MovingRoundedIcon from '@mui/icons-material/MovingRounded';
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 import HowToRegRoundedIcon from '@mui/icons-material/HowToRegRounded';
@@ -13,11 +13,11 @@ const handleClick = (value) =>{
 
 export function NavBar(){
     return (
-        <div className="side-div d-flex flex-column my-5 ">
-            <div className="logo-div mx-4 d-flex ">
+        <div className="navbar side-div d-flex flex-column  ">
+            <div className="logo-div mx-0 d-flex my-3 ">
                 <img className="logo-div-img rounded" src={innoviseLogo} alt="Innovise Logo"/>
             </div>
-            <div className="nav-div my-4 gap-3 d-flex flex-column">
+            <div className="nav-div  gap-3 d-flex flex-column">
                 <Button className="navbar-Button Poppins py-2" onClick={() => handleClick('Trending')}><MovingRoundedIcon/> &nbsp; Trending</Button>
                 <Button className="navbar-Button Poppins py-2" onClick={() => handleClick('Search')}><SearchRoundedIcon/> &nbsp; Search</Button>
                 <Button className="navbar-Button Poppins py-2" onClick={() => handleClick('Following')}><HowToRegRoundedIcon/> &nbsp; Following</Button>
