06071fa0f65351d33dfa5c7ef5a8c2e00ba090c0
Prthm444 <prathameshvasaikarat3x4@gmail.com>
2024-10-28 15:35:24 +0530

    ui changes

--- src/components/PostContainer.jsx
+++ src/components/PostContainer.jsx
@@ -95,7 +95,7 @@ export function PostContainer(props) {
 
     let postHtml = (
         <div style={{"display":"flex","justifyContent":"center","alignItems":"center"}}>
-            <img className="tw-opacity-25" src={not_found_src} alt="not-found-img" width="500vw"/>
+            <img className="tw-opacity-25 tw-scale-50" src={not_found_src} alt="not-found-img" width="500vw"/>
         </div>
     );
     if (postData.length > 0){
